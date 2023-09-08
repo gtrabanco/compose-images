312bb9625da3f4277358dd9b189cfea6455918be
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2023-09-08 14:31:07 +0200

    Convert shield as well

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
+import { basename, extname, join } from "path";
 import sharp from "sharp";
 import { addTextOnImage } from "./lib/add-text-on-image";
 import { positionAreaToBeCentered } from "./lib/position-to-be-centered-coords";
 import { resizeToProportional } from "./lib/resize-to-proportional";
 
+const understand_overwrite = (process.argv.includes('-y') || process.argv.includes('--yes') || process.argv.includes('--force'))
 
-
+if (!understand_overwrite) {
+  console.info('Any output file that exists will be overwritten');
+  console.warn('You should write -y as argument to make explicit that you understand this');
+  process.exit(-1);
+}
 
 
 
@@ -16,17 +22,18 @@ const CATEGORY_START_POSITION_COORDINATES: Coordinates2D = [639, 275]; // 3
 // const SHIELD_MAX_AREA: Area = [365, 370]; // 1
 // const SHIELD_MAX_AREA: Area = [310, 320]; // 2
 const VISITOR_SHIELD_MAX_AREA: Area = [450, 400]; // 3
-const WEEK_POSITION: Coordinates2D = [30, 820]
+const WEEK_POSITION: Coordinates2D = [30, 820];
 
 
 // Config files
 // const FONT_FILE_PATH = './assets/AlumniSans-Black.ttf'
 const BACKGROUND_IMAGE_PATH = "./assets/background3.png";
-const OUTPUT_FILE = process.env.HOME + "/Downloads/out.webp"
+const OUTPUT_PATH = process.env.HOME + "/Downloads/"; // file + extension will be added later. File will be the week
 
-const shieldFile = './assets/shields/Atletica.png';
+const shieldFile = './assets/shields/sdc_astillero.png';
 const categoryText = "PRIMERA NACIONAL";
-const WEEK = 1;
+const WEEK = 3;
+const SHOULD_OUTPUT_WEBP_SHIELD = true;
 
 const backgroundImage = sharp(await Bun.file(BACKGROUND_IMAGE_PATH).arrayBuffer());
 const shield = sharp(shieldFile);
@@ -40,25 +47,31 @@ if (originalWidth === 0 || originalHeight === 0) {
 const [rescaleWidth, rescaleHeight] = resizeToProportional([originalWidth, originalHeight], VISITOR_SHIELD_MAX_AREA);
 const [x, y] = positionAreaToBeCentered(VISITOR_SHIELD_START_POSITION_COORDINATES, VISITOR_SHIELD_MAX_AREA, [rescaleWidth, rescaleHeight]).map(Math.floor);
 
-let shieldConverted;
+let shieldResized;
 try {
   console.info('Shield')
   if (rescaleWidth !== originalWidth || rescaleHeight !== originalWidth) {
-    shieldConverted = sharp(await Bun.file(shieldFile).arrayBuffer()).resize({
+    shieldResized = sharp(await Bun.file(shieldFile).arrayBuffer()).resize({
       width: rescaleWidth,
       height: rescaleHeight,
     });
   } else {
-    shieldConverted = sharp(await Bun.file(shieldFile).arrayBuffer());
+    shieldResized = sharp(await Bun.file(shieldFile).arrayBuffer());
   }
 
-  // await shieldConverted.toFile('shield.webp');
-  // console.info('Shield converted');
+  console.info('Files will be writen in folder: %s', OUTPUT_PATH)
+  if (SHOULD_OUTPUT_WEBP_SHIELD) {
+    const filename = basename(shieldFile, extname(shieldFile)) + '.webp';
+    const webp_shield_path = join(OUTPUT_PATH, filename)
+    console.info('Converting shield to WEBP')
+    await shieldResized.webp({ quality: 100, lossless: true }).toFile(webp_shield_path);
+    console.info('Shield as WEBP done in: %s', webp_shield_path)
+  }
 
-  console.info('Starting composition')
-  const composition = await backgroundImage.webp({ quality: 100, lossless: true }).composite([
+  console.info('Configuring composition')
+  const composition = backgroundImage.composite([
     {
-      input: await shieldConverted.webp({ lossless: true, quality: 100 }).toBuffer(),
+      input: await shieldResized.webp({ lossless: true, quality: 100 }).toBuffer(),
       top: y,
       left: x,
     },
@@ -87,10 +100,18 @@ try {
     //   top: 500,
     //   left: 1050
     // }
-  ]).toBuffer();
-  Bun.write(OUTPUT_FILE, composition)
+  ]);
+  const FILENAME = `J${WEEK.toString().padStart(2, '0')}`
+  const FILE_PNG = join(OUTPUT_PATH, FILENAME + '.png')
+  const FILE_WEBP = join(OUTPUT_PATH, FILENAME + '.webp')
 
-  console.info('Composition done successfully: %s', OUTPUT_FILE)
+  console.info('Starting composition as PNG')
+  Bun.write(FILE_PNG, await composition.png({ quality: 100 }).toBuffer())
+  console.info('Composition as PNG done: %s', FILE_PNG)
+
+  console.info('Starting composition as WEBP')
+  Bun.write(FILE_WEBP, await composition.webp({ quality: 100, lossless: true }).toBuffer())
+  console.info('Composition as WEBP done: %s', FILE_WEBP)
 } catch (error) {
   console.warn(error)
 }
